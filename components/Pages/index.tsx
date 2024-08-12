763ce7c5c46c1f53d860edc57913b36038c1c109
SMHSaadat <hasanmicrob@gmail.com>
2024-08-12 23:43:58 +0330

    change style codes

--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -29,8 +29,8 @@ const Page: PageEl = (props, state, refresh, getProps) => {
           <div className='second-image-div'>
             <img src="/test2.png" alt="hi" />
           </div>
-          <div className='image-div'>
-            <img src="https://seeklogo.com/images/T/tether-usdt-logo-FA55C7F397-seeklogo.com.png" alt="hi" />
+          <div className='image-div' style={{backgroundColor:"rgba(25,25,112,0.95)"}}>
+            <img src="/weather.png" alt="" />
           </div>
 
           <div style={{
@@ -39,11 +39,12 @@ const Page: PageEl = (props, state, refresh, getProps) => {
             marginRight: 20, color: "white", 
           }}>
             <ul style={{ margin: 10, paddingTop: 30}}>
-              <li className='list-item time'>امروز: <span className='time'>{props.api_time.time24.hour.fa}:{props.api_time.time24.minute.fa}:{props.api_time.time24.second.fa} | {props.api_time.date.day.number.fa} {props.api_time.date.month.name} {props.api_time.date.year.number.fa}</span></li>
-              <li className="list-item price">دما: <span className="price">{parseFloat(props.current_condition.temp_C).toLocaleString("fa-IR")} سانتی گراد</span> | <span className='price'>{parseFloat(props.current_condition.temp_F).toLocaleString("fa-IR")} فارنهایت</span></li>
-              <li className="list-item price">دما: <span className="price">{parseFloat(props.current_condition.temp_C).toLocaleString("fa-IR")} سانتی گراد</span> | <span className='price'>{parseFloat(props.current_condition.temp_F).toLocaleString("fa-IR")} فارنهایت</span></li>
-              <li className="list-item price">دما: <span className="price">{parseFloat(props.current_condition.temp_C).toLocaleString("fa-IR")} سانتی گراد</span> | <span className='price'>{parseFloat(props.current_condition.temp_F).toLocaleString("fa-IR")} فارنهایت</span></li>
-              <li className="list-item price">دما: <span className="price">{parseFloat(props.current_condition.temp_C).toLocaleString("fa-IR")} سانتی گراد</span> | <span className='price'>{parseFloat(props.current_condition.temp_F).toLocaleString("fa-IR")} فارنهایت</span></li>
+              <li className='list-item time hover' style={{fontSize:"140%"}}><span className='time'>{props.api_time.time24.hour.fa}:{props.api_time.time24.minute.fa}:{props.api_time.time24.second.fa} | {props.api_time.date.day.number.fa} {props.api_time.date.month.name} {props.api_time.date.year.number.fa}</span> |<span className='time'>🌇{(parseFloat((props.weather.astronomy[0].sunrise).slice(0, 2)).toLocaleString("fa-IR"))}:{(parseFloat((props.weather.astronomy[0].sunrise).slice(3, 5)).toLocaleString("fa-IR"))}</span> |<span className='time'>🌆{(parseFloat((props.weather.astronomy[0].sunset).slice(0, 2)).toLocaleString("fa-IR"))}:{(parseFloat((props.weather.astronomy[0].sunset).slice(3, 5)).toLocaleString("fa-IR"))}</span></li>
+              <li className="list-item location hover"><span className="weather c-blue">ایران</span> | <span className='c-blue'>فارس</span> | <span className='c-blue'>شیراز</span> | <span className='c-blue'>{parseFloat(props.area.population).toLocaleString("fa-IR")} نفر</span></li>
+              <li className='list-item weather-info hover'><span className='c-blue'>هوا: آفتابی</span> | <span className='c-blue'>😎 سطح اشعه فرابنفش: {parseFloat(props.current_condition.uvIndex).toLocaleString("fa-IR")}</span></li>
+              <li className="list-item weather hover"><span className="c-blue">{parseFloat(props.current_condition.temp_C).toLocaleString("fa-IR")} سانتی گراد</span> | <span className='c-blue'>⬇️{parseFloat(props.weather.mintempC).toLocaleString("fa-IR")}</span> | <span className='c-blue'>⬆️{parseFloat(props.weather.maxtempC).toLocaleString("fa-IR")}</span></li>
+              <li className='list-item weather-info hover'><span className='c-blue'>رطوبت: {parseFloat(props.current_condition.humidity).toLocaleString("fa-IR")}٪</span> |<span className='c-blue'>🍃وزش باد: {parseFloat(props.current_condition.windspeedKmph).toLocaleString('fa-IR')}</span><span className='c-blue' style={{fontSize:"15px"}}>کیلومتربرساعت</span></li>
+
             </ul>
           </div>
 
@@ -69,14 +70,20 @@ export async function getServerSideProps(context) {
     let api_time = await time_response.json()
     let data = await response.json()
     let current_condition = data.current_condition[0]
-    let area = null
-    let weather = null
+    let area = data.nearest_area[0]
+    let weather = data.weather[0]
+    let sunrise = data.weather[0].astronomy[0].sunrise
+
+    console.log(sunrise.slice(0, 5))
+
 
     
     return {
       props: {
         data: global.QSON.stringify({
           current_condition:current_condition,
+          area:area,
+          weather,
           session,
           api_time:api_time,
           // nlangs,
